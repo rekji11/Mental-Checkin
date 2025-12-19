f9eed82498676649f543e129b0c501fb1bf50677
Jerick De Guzman <jerickdeguzman1030@gmail.com>
2025-12-19 16:00:22 +0800

    React

--- main.py
+++ main.py
@@ -4,8 +4,17 @@ from app import models
 from app.routes import auth_routes
 from app.routes import users_routes 
 models.Base.metadata.create_all(bind=engine)
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Mental Health Check-in API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(auth_routes.router)
